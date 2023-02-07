4a4d8eae1a0340d5d12deccd7adae8e8eeef39f7
Jorge <jorge.ar.dominguez@hotmail.com>
2023-02-07 17:24:57 +0100

    Cambios2

--- Javascript/Ejercicio8.js
+++ Javascript/Ejercicio8.js
@@ -1,17 +1,13 @@
 const letrasDNI = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E','T'];
 let numeroDNI = prompt("Introduce tu número de DNI","99999999");
-var resultadoDNI = 0;
-var letraDNI = letrasDNI[resultadoDNI];
+var letraDNI;
 
-if (numeroDNI < 0 || numeroDNI > 99999999) {
-    alert("El número especificado no es válido");
-}   else if (numeroDNI.length !=  8) {
+if (numeroDNI < 0 || numeroDNI > 99999999 ||numeroDNI.length !=8) {
     alert("El número especificado no es válido");
 }   else {
     var resultadoDNI = numeroDNI%23;
-    var letraDNI = letrasDNI[resultadoDNI]
-    console.log(resultadoDNI)
-}
+    letraDNI = letrasDNI[resultadoDNI]
+    }
 
 console.log(letraDNI)
 alert(letraDNI)
